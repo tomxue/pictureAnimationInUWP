a2b6c142ce0588169daf6dcc82e0d1860f6517ff
tomxue <tomxue@outlook.com>
2020-01-07 16:51:43 +0800

    work with Nico Zhu's code

--- App12/MainPage.xaml.cs
+++ App12/MainPage.xaml.cs
@@ -27,7 +27,12 @@ namespace App12
             this.InitializeComponent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void Image1_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
         {
             std2.Begin();
         }
